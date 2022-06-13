d618c3742d039698003fe4479d22f1977e81f0ed
mtuong79 <89337936+mtuong79@users.noreply.github.com>
2022-06-13 22:38:40 +0700

    Create pymongo.py

---
+++ pymongo.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pymongo
+import ssl
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+# @st.experimental_singleton
+
+uri = "mongodb+srv://mtuong:Wellcome2@oucru-it.bcq3ord.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(uri)
+
+try:
+    st.write(client.server_info())
+except Exception:
+    st.write("Unable to connect to the server.")
+
+# Pull data from the collection.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+# @st.experimental_memo(ttl=600)
+def get_data():
+    db = client.InkMgmt
+    items = db.mycollection.find()
+    items = list(items)  # make hashable for st.experimental_memo
+    return items
+
+items = get_data()
+
+# Print results.
+for item in items:
+    st.write(f"{item['name']} has a :{item['beds']}:")
